c822b548b9b1a31edc17181d9402667bbe795227
POCCrossPlatformDISFE <POCCrossPlatformDISFE@laposte.net>
2016-09-05 18:11:30 +0200

    Correction de l'import des headers

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-// Add headers
-app.use(function (req, res, next) {
+// // Add headers
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://disfecarrouseldemo.herokuapp.com/');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://disfecarrouseldemo.herokuapp.com/');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 app.get('/', function(request, response) {
-  var headers = {
-    'Access-Control-Allow-Origin' : 'https://disfecarrouseldemo.herokuapp.com/',
-    'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    'Access-Control-Allow-Headers' : 'X-Requested-With,content-type'
-  }  
-  response.sendFile('La_Poste_WiFi.html',headers);
+  // var headers = {
+  //   'Access-Control-Allow-Origin' : 'https://disfecarrouseldemo.herokuapp.com/',
+  //   'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  //   'Access-Control-Allow-Headers' : 'X-Requested-With,content-type'
+  // }  
+  response.sendFile('La_Poste_WiFi.html');
 });
 
 app.listen(app.get('port'), function() {
