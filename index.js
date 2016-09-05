cab685c14dfa0c095d83a05c60dd12558cffe718
POCCrossPlatformDISFE <POCCrossPlatformDISFE@laposte.net>
2016-09-05 16:48:32 +0200

    Correction des headers sur le serveur répondant

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
+// on précise ici qu'on autorise toutes les sources
+// puis dans le second header, quels headers http sont acceptés
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/', function(request, response) {
-  response.header("Access-Control-Allow-Origin", "https://disfecarrouseldemo.herokuapp.com");
-
+app.get('/', function(request, response) {  
+  response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   response.sendFile('La_Poste_WiFi.html');
 });
 
