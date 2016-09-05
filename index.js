5d447d3a114e8a6630789372bbbfcec7153afce3
POCCrossPlatformDISFE <POCCrossPlatformDISFE@laposte.net>
2016-09-05 17:36:59 +0200

    Changement des headers

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-// on précise ici qu'on autorise toutes les sources
-// puis dans le second header, quels headers http sont acceptés
-app.use(function(request, response, next) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://disfecarrouseldemo.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.get('/', function(request, response) {  
