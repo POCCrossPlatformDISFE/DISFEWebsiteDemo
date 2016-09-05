9d51f31f7d0d81deaa836729d816980790f08a24
POCCrossPlatformDISFE <POCCrossPlatformDISFE@laposte.net>
2016-09-05 16:35:09 +0200

    Correction de l'intégration des headers

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header("Access-Control-Allow-Origin", "https://disfecarrouseldemo.herokuapp.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/', function(request, response) {
+  response.header("Access-Control-Allow-Methods", "GET");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Origin", "https://disfecarrouseldemo.herokuapp.com");
+  
+
   response.sendFile('La_Poste_WiFi.html');
 });
 
