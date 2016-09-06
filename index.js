c96f76f960522acf87a619d866cf67f106f44dcf
POCCrossPlatformDISFE <POCCrossPlatformDISFE@laposte.net>
2016-09-06 14:40:28 +0200

    Ajout des routes par type d'utilisateur

--- index.js
+++ index.js
@@ -12,27 +12,16 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-// // Add headers
-// app.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'https://disfecarrouseldemo.herokuapp.com/');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 app.get('/', function(request, response) {
+  response.sendFile('La_Poste_WiFi.html');
+});
+
+app.get('/particulier', function(request, response) {
+  response.sendFile('La_Poste_WiFi.html');
+});
+
+app.get('/professionnel', function(request, response) {
   response.sendFile('La_Poste_WiFi.html');
 });
 
